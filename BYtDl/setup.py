c27ee0ccbf042c61c7d4510fca84d2d32cb35304
0300dbdd1b <noah.gr.addino@gmail.com>
2024-07-09 21:50:07 +0200

    Init

---
+++ BYtDl/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BYtDl',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'yt-dlp',
+        'textual'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bytdl=BYtDl.BYtDl:main',
+        ],
+    },
+    author='0300dbdd1b',
+    author_email='_@_',
+    description='yt-dlp with a Textual interface.',
+    url='https://github.com/0300dbdd1b/BYtDl',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
